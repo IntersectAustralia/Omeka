4e7eea2abcac52cc01a82955946024ddd6d483b4
Cindy <cindy@intersect.org.au>
2014-11-17 13:42:27 +1100

    SNAPDEPLOY-174: Embed AAF login form inside existing Omeka theme.

--- application/views/scripts/aaf.php
+++ application/views/scripts/aaf.php
@@ -1,8 +1,12 @@
+<?php
+$pageTitle = __('Log In Procedure');
+echo head(array('bodyclass' => 'login', 'title' => $pageTitle), $header);
+?>
 <h2>Log In Procedure</h2>
 <p> The Australian Access Federation (AAF) provides a framework to facilitate trusted electronic communications and collaboration within and between universities and research institutions in Australia.
     You will need an AAF login to access private collections or items in Omeka.
 <p> Once you have attained AAF credentials, follow these steps to log in:
-	<li>Click the 'Australian Access Federation' button below</li>
-    <li>Select your organisation, then click 'Select'</li>
+<li>Click the 'Australian Access Federation' button below</li>
+<li>Select your organisation, then click 'Select'</li>
 
 <a href="https://rapid.test.aaf.edu.au/jwt/authnrequest/research/dNfCG6QH5WVLIcaJohK2eg" class="btn btn-large btn-info"><img src="https://rapid.aaf.edu.au/aaf_service_223x54.png" border="0"></a>
